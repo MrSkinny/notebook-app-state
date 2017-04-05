afd70711ac874877106c22e7b02a5ff6d91f70cc
Rich Greenhill <rgnet1@yahoo.com>
2017-04-05 11:40:08 -0700

    create state obj

---
+++ app.js
@@ -0,0 +1,69 @@
+const DATABASE = ;
+
+const appState = {
+    // typically, `allNotebooks` would be pulled from a database and not stored in state
+    allNotebooks: [
+        {
+            id: 100,
+            label: 'Personal',
+            notes: [
+                {
+                    id: 200,
+                    title: 'Shopping',
+                    body: 'This is a note about shopping. Maybe I will put a list of stuff to buy here. For example purposes, we want to keep notes to a single paragraph.'
+                },
+                {
+                    id: 201,
+                    title: 'Doctors no.',
+                    body: '455-555-3232 appointment on 6/12'
+                },
+                {
+                    id: 202,
+                    title: 'Call with mom',
+                    body: "She needs me to clean out my old clothes, they're turning bedroom into an exercise room...!"
+                },
+            ]
+        },
+        {
+            id: 101,
+            label: 'Work',
+            notes: [
+                {
+                    id: 203,
+                    title: 'Meeting with Sales',
+                    body: 'Need to up the quarterly projections ASAP'
+                },
+                {
+                    id: 204,
+                    title: 'Relocation',
+                    body: 'Looking at 3 diff locations for the extension office; need space for 15 people'
+                },
+                {
+                    id: 205,
+                    title: 'Dept. memos',
+                    body: "We're not sending out enough memos for people to keep up with the important goings-ons in and around the office. Meet with Bhaumik, Joe to increase memo output 5x"
+                }
+            ]
+        }
+    ],
+    selectedNotebook: {
+        id: 100,
+        label: 'Personal',
+        notes: [
+            {
+                id: 200,
+                title: 'Shopping'
+            },
+            {
+                id: 201,
+                title: 'Doctors no.'
+            },
+            {
+                id: 202,
+                title: 'Call with mom'
+            }
+        ]
+    },
+    selectedNote: null,
+    isAddingNote: false
+};
